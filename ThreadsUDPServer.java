1088a42b9eaab127a35d3c7ce701c463bff77ee6
bnass <bnass14@student.mail.dk>
2015-11-04 14:59:32 +0100

    Brian Commit 4 nov 2015

--- ThreadsUDPServer.java
+++ ThreadsUDPServer.java
@@ -1,64 +1,71 @@
-import com.google.gson.Gson;
+//import com.google.gson.Gson;
 import java.io.IOException;
 import java.net.*;
 
-
 public class ThreadsUDPServer {
+
+	static byte[] data;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int BUFFERSIZE = 50;
-		
-		TestClass tc5 = new TestClass();
-		tc5.name = "Peter";
-		tc5.age = 15;
-		
-		
-		Gson g1 = new Gson();
-		
-		String json2 = g1.toJson(tc5);
-				
-		
-		DatagramPacket pack = new DatagramPacket(new byte[BUFFERSIZE],BUFFERSIZE);
+
+		final int BUFFERSIZE = 50;
+
 		DatagramSocket sock = null;
-		
-		BUFFERSIZE = pack.getLength();
-		
+
 		try {
 			sock = new DatagramSocket(2000);
-		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		//Multithread mt1 = new Multithread(sock,pack);
-		
-		//mt1.start();
-		try {
-			sock.receive(pack);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		BUFFERSIZE = pack.getLength();
-		
-		String json = new String(pack.getData());
-		
-		
-		System.out.println(json);
-		
-		
-		//TestClass tc1 = g1.fromJson(json, TestClass.class);
-		
-		//System.out.println(tc1.name);
-		
-		//System.out.println(new String(pack.getData()));
-		
-		
 
+			// Multithread mt1 = new Multithread(sock,pack);
+
+			// mt1.start();
+			while (true)
+
+			{
+
+				String json = "";
+				String IPAdress = "";
+
+				data = new byte[BUFFERSIZE];
+
+				DatagramPacket pack = new DatagramPacket(data, BUFFERSIZE);
+
+				sock.receive(pack);
+				
+				IPAdress = new String("IP: " + pack.getAddress());
+
+				json = new String(pack.getData());
+				
+				
+				System.out.println(json + " " + IPAdress );
+				
+
+				if (json.startsWith("quit")) {
+					System.out.println("quitting");
+
+					break;
+				}
+			}
+
+			sock.close();
+
+		}
+
+		catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
 }
+
+// TestClass tc1 = g1.fromJson(json, TestClass.class);
+
+// System.out.println(tc1.name);
+
+// System.out.println(new String(pack.getData()));
+
